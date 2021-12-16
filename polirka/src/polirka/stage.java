02c1b208105b3579755107b394627a9c020e8558
Dejan <hans.lamele@gmail.com>
2021-12-16 16:47:41 +0100

    Add files via upload

---
+++ polirka/src/polirka/stage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polirka;
+
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author Dejan
+ */
+class stage {
+
+    static void setScene(Scene scene) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
